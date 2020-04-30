3be9cf368c538952a14bf8d4f03e9f9211921442
sarahschwaiger <sarahschwaiger@172.20.10.2>
2020-05-01 00:48:00 +0200

    Great commit

---
+++ src/forgithubmain/class2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forgithubmain;
+
+/**
+ *
+ * @author sarahschwaiger
+ */
+public class class2 {
+    public void m(){
+        System.out.println("In m");
+    }
+}
