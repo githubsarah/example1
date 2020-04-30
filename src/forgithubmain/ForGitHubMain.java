408c82a9e4301f4c62dd43dcccc7da66ae4e5978
sarahschwaiger <sarahschwaiger@172.20.10.2>
2020-05-01 00:47:29 +0200

    Great commit

---
+++ src/forgithubmain/ForGitHubMain.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forgithubmain;
+
+/**
+ *
+ * @author sarahschwaiger
+ */
+public class ForGitHubMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        class2 c = new class2();
+        c.m();
+    }
+    
+}
